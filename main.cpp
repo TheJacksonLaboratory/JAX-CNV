ee2a0d2e9c7128f0b19b0c7acdb11c7c8ddfc2a6
Wan-ping Lee <wanping.lee@gmail.com>
2017-09-19 18:02:55 -0400

    Load jellyfish as jellyfish database.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include "fasta.h"
 #include "reference.h"
 #include "jellyfish/file_header.hpp"
+#include "jellyfish/jellyfish.hpp"
+#include "jellyfish/mapped_file.hpp"
 
 int main (int argc, char** argv) {
 	if (argc != 3) {
@@ -13,18 +15,32 @@ int main (int argc, char** argv) {
 		return 1;
 	}
 
+	// Read jellyfish database
 	std::ifstream db(argv[1], std::ios::in|std::ios::binary);
 	jellyfish::file_header header(db);
-	if(!db.good()) {
-		std::cerr << "Cannot open " << argv[1] << std::endl;
+	if(!db.good()) { // The jellyfish database is broken.
+		std::cerr << "ERROR: Cannot open " << argv[1] << std::endl;
+		return 1;
+	}
+	if (header.format() != binary_dumper::format) {
+		std::cerr << "ERROR: Cannot process jellyfish database built by bloom filter." << std::endl;
+		return 1;
+	}
+	jellyfish::mapped_file binary_map(argv[1]);
+	binary_map.load(); // Load in memory for speedup.
+	binary_query bq(binary_map.base() + header.offset(), header.key_len(), header.counter_len(), header.matrix(),
+				header.size() - 1, binary_map.length() - header.offset());
+	
+
+	// Read kmer size from the header of jellyfish database
+	const int kmer_size = header.key_len() / 2; // The kmer size is key_len() / 2.
+	if (kmer_size < 1) { // Cannot proceed if kmer size is not larger than zero.
+		std::cerr << "ERROR: The kmer size (" << kmer_size << ") should be larger than 0." << std::endl;
 		return 1;
 	}
 
-	const int kmer_size = header.key_len() / 2;
-	std::cerr << kmer_size << std::endl;
-
-	CReference ref;
-	Fasta::Load(ref, argv[2]);
+	CReference ref; // fastaq lib.
+	Fasta::Load(ref, argv[2]); // fastaq lib.
 	std::vector<std::string> ref_names;
 	ref.GetReferenceNames(&ref_names);
 	for (unsigned int i = 0; i < ref_names.size(); i++) {
