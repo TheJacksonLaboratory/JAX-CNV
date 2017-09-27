fee2abb7f8ebbea775af555a3c8954a6663feec8
Wan-ping Lee <wanping.lee@gmail.com>
2017-09-27 10:23:19 -0400

    Create new main.cpp for sub command structure.

---
+++ include/CountKmer.h
@@ -0,0 +1,102 @@
+#ifndef _COUNTKMER_H_
+#define _COUNTKMER_H_
+
+#include <getopt.h>
+#include <string>
+
+struct SCmdLine {
+	SCmdLine(const int argc, char** const argv){Parse(argc, argv);}
+
+	bool help = false;
+
+	// i/o
+	std::string input_jfdb; // -i --input
+	std::string fasta;      // -f --fasta
+	std::string bam;	// -b --bam
+	std::string output;     // -o --output
+
+	// operation parameters
+	std::string region;     // -r --region
+	int bin = 1;
+	bool running_length_encoding = false; // --rle
+	
+	// command line
+	std::string cmd;
+
+	const char* short_option = "hi:f:b:o:r:";
+
+	// Help list
+	const std::string Help (const char* program) const { return
+		std::string("USAGE: ") + program + std::string(" -i <jellyfish_db> -f <FASTA>\n\n") +
+		std::string("	-h --help			Print this help list.\n") +
+		std::string("\n") +
+		std::string("Input & Output:\n") +
+		std::string("	-i --input <jellyfish_db>	Jellyfish created count database.\n") +
+		std::string("	-f --fasta <FASTA>		FASTA for kmer lookup.\n") +
+		std::string("	-b --bam <BAM>			Input BAM.\n") +
+		std::string("	-o --output <FILE>		Output file.\n") +
+		std::string("\n") +
+		std::string("Operations:\n") +
+		std::string("	-r --region chr:begin-end	Specify a target region.\n") +
+		std::string("	--bin <INT>			Report a result for each # bp. [1]\n") +
+		std::string("	--rle				Ouput by running length encoding.\n");
+	}
+
+	// Check the required arguments.
+	bool CheckArg () const {
+		bool ok = true;
+		if (bin < 1) {
+			std::cerr << "ERROR: --bin <INT> should not smaller than 1." << std::endl;
+			ok = false;
+		}
+		if (bin > 1 && running_length_encoding) {
+			std::cerr << "ERROR: --rle only work for --bin 1." << std::endl;
+			ok = false;
+		}
+
+		return ok && ((!input_jfdb.empty() && !fasta.empty()) || !bam.empty());
+	}
+
+	bool Parse (const int argc, char** const argv) {
+		// Record the input command line.
+		for (int i = 0; i < argc; i++) cmd += std::string(argv[i]) + " ";
+		const struct option long_option[] = {
+			{"help", required_argument, NULL, 'h'},
+			// i/o
+			{"input", required_argument, NULL, 'i'},
+			{"fasta", required_argument, NULL, 'f'},
+			{"bam", required_argument, NULL, 'b'},
+			{"output", required_argument, NULL, 'o'},
+
+			// operation parameters
+			{"region", required_argument, NULL, 'r'},
+			{"bin", required_argument, NULL, 1},
+			{"rle", no_argument, NULL, 2},
+			{0,0,0,0}
+		};
+		int option_index = 0;
+		int c = -1;
+		while ((c = getopt_long(argc, argv, short_option, long_option, &option_index)) != -1) {
+			switch (c) {
+				case 'h': help = true; break;
+				case 'i': input_jfdb = optarg; break;
+				case 'f': fasta = optarg; break;
+				case 'b': bam = optarg; break;
+				case 'o': output = optarg; break;
+				case 'r': region = optarg; break;
+				case 1: bin = atoi(optarg); break;
+				case 2: running_length_encoding = true; break;
+				default: std::cerr << "WARNING: Unkonw parameter: " << long_option[option_index].name << std::endl; break;
+			}
+		}
+
+		if (help) {
+			Help(argv[0]);
+			return false;
+		}
+
+		return CheckArg();
+	}
+};
+
+#endif
---
+++ src/CountKmer.cpp
@@ -0,0 +1,298 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+// Self include
+#include "include/CommandLine.h"
+
+// FASTAQ include
+#include "fasta.h"
+#include "reference.h"
+#include "region.h"
+
+// Jellyfish include
+#include "jellyfish/file_header.hpp"
+#include "jellyfish/jellyfish.hpp"
+#include "jellyfish/mapped_file.hpp"
+
+// htslib include
+#include "htslib/sam.h"
+
+// Calculate log2 score.
+char CeilLog2 (const uint64_t in)
+{
+	uint64_t x = in;
+	static const unsigned long long t[6] = {
+		0xFFFFFFFF00000000ull,
+		0x00000000FFFF0000ull,
+		0x000000000000FF00ull,
+		0x00000000000000F0ull,
+		0x000000000000000Cull,
+		0x0000000000000002ull
+	};
+
+	//int y = (((x & (x - 1)) == 0) ? 0 : 1);
+	int y = (x > 0) ? 1 : 0;
+
+	for (int i = 0, j = 32; i < 6; i++) {
+		int k = (((x & t[i]) == 0) ? 0 : j);
+		y += k;
+		x >>= k;
+		j >>= 1;
+	}
+
+	// Use 125 as the max.
+	return (y > 92) ? 125 : y + 33;
+}
+
+void GetKmerCount (const Fastaq::CReference & ref, const Fastaq::SRegion & region, const unsigned int & kmer_size, 
+			const jellyfish::file_header & header, const binary_query & bq, const bool & running_length_encoding,
+			const int & bin) {
+
+	if (bin < 1) return;
+
+	std::vector<std::string> ref_names;
+	ref.GetReferenceNames(&ref_names);
+	// If a region is given, there will be only one reference in the vector.
+	for (unsigned int i = 0; i < ref_names.size(); i++) {
+		const unsigned int target_len = std::min(ref.GetReferenceLength(ref_names[i].c_str()), region.end);
+		const unsigned int target_begin = std::max(static_cast<unsigned int>(0), region.begin);
+
+		// Cannot proceed when target_len < kmer_size
+		if (target_len < kmer_size) break;
+
+		//if (cmdline.region.empty()) // Ouput refernce name only if a region is not given.
+		//	std::cout << ">" << ref_names[i] << std::endl;
+
+		unsigned int score_count = 0;
+		unsigned int score_sum = 0;
+		char score = '\0';
+		for (unsigned int j = target_begin; j < target_len - kmer_size; ++j) {
+			jellyfish::mer_dna m;
+			m = ref.GetSubString(ref_names[i], j, kmer_size).c_str();
+			if (header.canonical()) m.canonicalize();
+			if (running_length_encoding) {
+				if (CeilLog2(bq.check(m)) == score) {
+					++score_count;
+				} else {
+					if (score != '\0')
+						std::cout << score << "\t" << score_count << std::endl;
+					score_count = 1;
+					score = CeilLog2(bq.check(m));
+				}
+			} else { // not running_length_encoding
+				score_sum += CeilLog2(bq.check(m));
+				if (((j-target_begin + 1) % bin) == 0) {
+					std::cout << static_cast<char>(score_sum / bin);
+					score_sum = 0;
+				}
+			}
+		}
+		// Output the last score. Only running_length_encoding mode will use this.
+		if (score_count > 0)
+			std::cout << score << "\t" << score_count << std::endl;
+		if (!running_length_encoding)
+			std::cout << std::endl;
+	}
+}
+
+struct SBamData {
+	unsigned int total_read = 0;
+	unsigned int proper_pairs = 0;
+	unsigned int inproper_pairs = 0;
+	unsigned int mate_unmapped = 0;
+	std::vector<unsigned int> poss;
+	std::vector<unsigned int> rds;
+	std::vector<unsigned int> isizes;
+
+	void Clean() {
+		total_read = 0;
+		proper_pairs = 0;
+		inproper_pairs = 0;
+		mate_unmapped = 0;
+		poss.clear();
+		rds.clear();
+		isizes.clear();
+	}
+};
+
+void PrintBamData (const SBamData & bam_data) {
+	if (bam_data.total_read == 0) {
+		std::cout << "0\t0\t0\t0" << std::endl;
+	} else {
+		std::cout << bam_data.proper_pairs / static_cast<double>(bam_data.total_read) << "\t"
+				<< bam_data.inproper_pairs / static_cast<double>(bam_data.total_read) << "\t"
+				<< bam_data.mate_unmapped / static_cast<double>(bam_data.total_read) << "\t";
+	
+		uint64_t sum = 0;
+		for (unsigned int i = 0; i < bam_data.isizes.size(); ++i)
+			sum += bam_data.isizes[i];
+		std::cout << sum / static_cast<double>(bam_data.total_read) << std::endl;
+	}
+};
+
+void ProcessAlignment (SBamData & bam_data, const bam1_t * aln) {
+	if (aln->core.flag & BAM_FUNMAP) return;
+	++bam_data.total_read;
+	if (aln->core.flag & BAM_FPROPER_PAIR) ++bam_data.proper_pairs;
+	else ++bam_data.inproper_pairs;
+	if (aln->core.flag & BAM_FMUNMAP) ++bam_data.mate_unmapped;
+	else bam_data.isizes.push_back(aln->core.isize < 0 ? -aln->core.isize : aln->core.isize);
+
+			//int32_t pos = aln->core.pos +1; //left most position of alignment in zero based coordianate (+1)
+			//char *chr = header->target_name[aln->core.tid] ; //contig name (chromosome)
+			//uint32_t len = aln->core.l_qseq; //length of the read.
+			//uint8_t *q = bam_get_seq(aln); //quality string
+			//uint32_t q2 = aln->core.qual ; //mapping quality
+}
+
+void ProcessBam (const char * bam_filename, const Fastaq::SRegion & region, const int & bin) {
+	samFile * bam_reader = sam_open(bam_filename, "r");
+
+	bam_hdr_t *header;
+	header = sam_hdr_read(bam_reader);
+	bam1_t * aln = bam_init1();
+
+	SBamData bam_data;
+
+	if (region.chr.empty()) { // the region is not set
+		int pre_bin = 0;
+		int pre_tid = 0;
+		while (sam_read1(bam_reader, header, aln) >= 0) {
+			// Enter a new chr.
+			if (aln->core.tid != pre_tid) {
+				pre_bin = 0;
+				pre_tid = aln->core.tid;
+			}
+			const int cur_bin = aln->core.pos / bin;
+			if (cur_bin != pre_bin) {
+				for (int i = pre_bin; i < cur_bin - 1; ++i) {
+					SBamData dummy_data;
+					PrintBamData(dummy_data);
+				}
+				PrintBamData(bam_data);
+				bam_data.Clean();
+				pre_bin = cur_bin;
+			}
+			ProcessAlignment(bam_data, aln);
+		}
+	} else { // the region is given.
+		hts_idx_t * idx = sam_index_load(bam_reader,  bam_filename);
+		bool load_index = true;
+		if (idx == NULL) {
+			if (sam_index_build(bam_filename, 0) < 0) { // Try to build bam index
+				std::cerr << "ERROR: The region givin but bam index cannot be built and loaded." << std::endl;
+				load_index = false;
+			} else {
+				idx = sam_index_load(bam_reader,  bam_filename);
+			}
+			
+		}
+
+		if (load_index) {
+			const std::string cat_region = region.chr + ":" + std::to_string(region.begin) + '-' +  std::to_string(region.end);
+			hts_itr_t * iter = sam_itr_querys(idx, header, cat_region.c_str());
+			int pre_bin = region.begin / bin;
+			while (iter && sam_itr_next(bam_reader, iter, aln) >= 0) {
+				const int cur_bin = aln->core.pos / bin;
+				if ((cur_bin > pre_bin) && (cur_bin != pre_bin)) {
+					for (int i = pre_bin; i < cur_bin - 1; ++i) {
+						SBamData dummy_data;
+						PrintBamData(dummy_data);
+					}
+					PrintBamData(bam_data);
+					bam_data.Clean();
+					pre_bin = cur_bin;
+				}
+				ProcessAlignment(bam_data, aln);
+			}
+	
+			// Clean up
+			hts_itr_destroy(iter);
+		}
+		
+	}
+
+	PrintBamData(bam_data);
+
+	// Clean up
+	bam_destroy1(aln);
+	bam_hdr_destroy(header);
+	sam_close(bam_reader);
+}
+
+int main (int argc, char** argv) {
+	const SCmdLine cmdline(argc, argv);
+	if (!cmdline.CheckArg()) {
+		std::cerr << cmdline.Help(argv[0]);
+		return 1;
+	}
+
+	// Read jellyfish database
+	std::ifstream db(cmdline.input_jfdb, std::ios::in|std::ios::binary);
+	jellyfish::file_header header(db);
+	if(!db.good()) { // The jellyfish database is broken.
+		std::cerr << "ERROR: Cannot open " << cmdline.input_jfdb << std::endl;
+		return 1;
+	}
+	if (header.format() != binary_dumper::format) {
+		std::cerr << "ERROR: Cannot process jellyfish database built by bloom filter." << std::endl;
+		return 1;
+	}
+	
+	// Read kmer size from the header of jellyfish database
+	const unsigned int kmer_size = header.key_len() / 2; // The kmer size is key_len() / 2.
+	jellyfish::mer_dna::k(kmer_size);
+	if (kmer_size < 1) { // Cannot proceed if kmer size is not larger than zero.
+		std::cerr << "ERROR: The kmer size (" << kmer_size << ") should be larger than 0." << std::endl;
+		return 1;
+	}
+
+	// Load jellyfish database as query db.
+	jellyfish::mapped_file binary_map(cmdline.input_jfdb.c_str());
+	binary_map.load(); // Load in memory for speedup.
+	binary_query bq(binary_map.base() + header.offset(), header.key_len(), header.counter_len(), header.matrix(),
+				header.size() - 1, binary_map.length() - header.offset());
+
+	// Parse region.
+	Fastaq::SRegion region;
+	if (!cmdline.region.empty()) {
+		if (!region.Parse(cmdline.region)) {
+			std::cerr << "ERROR: The given region is not valid." << std::endl;
+			return 1;
+		}
+	}
+
+	// Load reference from FASTA.
+	Fastaq::CReference ref; // fastaq lib.
+	if (!cmdline.region.empty()) {
+		if (!Fastaq::FastaLoad(ref, cmdline.fasta.c_str(), true, region.chr.c_str())) {
+			std::cerr << "ERROR: Cannot load chromosome " << region.chr << " from FASTA." << std::endl;
+			return 1;
+		}
+	} else {
+		if (!Fastaq::FastaLoad(ref, cmdline.fasta.c_str())) {
+			std::cerr << "ERROR: Cannot load FASTA." << std::endl;
+			return 1;
+		}
+	}
+
+	// Open output if given.
+	std::ofstream ofs;
+	std::streambuf *coutbuf = std::cout.rdbuf(); //save old buf
+	if (!cmdline.output.empty()) {
+		ofs.open(cmdline.output, std::ofstream::out | std::ofstream::app);
+		std::cout.rdbuf(ofs.rdbuf()); //redirect std::cout to file;
+	}
+
+	GetKmerCount(ref, region, kmer_size, header, bq, cmdline.running_length_encoding, cmdline.bin);
+	ProcessBam(cmdline.bam.c_str(), region, cmdline.bin);
+
+	// Clean up
+	if (!cmdline.output.empty()) {
+		std::cout.rdbuf(coutbuf); //reset to standard output again
+		ofs.close();
+	}
+
+}
