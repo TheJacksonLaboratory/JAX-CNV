3ed555ae6334104b3f6f7d247adfe09d3b0250ab
Wan-Ping Lee <wanping.lee@gmail.com>
2019-06-13 02:22:13 -0400

    Remove "GenerateKmer" sub command.

--- main.cpp
+++ main.cpp
@@ -6,19 +6,20 @@
 // Self include
 #include "GrabJellyfishKmer.h"
 #include "GetCnvSignal.h"
-#include "GenerateKmer.h"
+//#include "GenerateKmer.h"
 
 struct SubCommand {
-	const unsigned int no_sub_commands = 3;
-	const std::string sub_commands[3] = {"GrabJellyfishKmer", "GetCnvSignal", "GenerateKmer"};
+	const unsigned int no_sub_commands = 2;
+	//const std::string sub_commands[3] = {"GrabJellyfishKmer", "GetCnvSignal", "GenerateKmer"};
+	const std::string sub_commands[2] = {"GrabJellyfishKmer", "GetCnvSignal"};
 
 	const std::string Help (const char* program) const { return
 		std::string("\n") +
 		std::string("USAGE: ") + program + std::string(" <command> [options]\n\n") +
 		std::string("Commands:\n") +
 		std::string("\tGrabJellyfishKmer	Report the count of kmer giving Jellyfish database and a FASTA.\n") +
-		std::string("\tGetCnvSignal	Report CNV signals such as read depth and kmer count.\n")+
-		std::string("\tGenerateKmer	Generate a kmer table by giving a FASTA. The kmer is shown by ascii code of log2(kmer)+34.\n");
+		std::string("\tGetCnvSignal	Report CNV signals such as read depth and kmer count.\n");
+		//std::string("\tGenerateKmer	Generate a kmer table by giving a FASTA. The kmer is shown by ascii code of log2(kmer)+34.\n");
 	}
 };
 
@@ -50,10 +51,10 @@ int main (int argc, char** argv) {
 			GetCnvSignal get_cnv_signal(argc - 1, argv + 1);
 			get_cnv_signal.Run();
 		}
-		else if (command == "GenerateKmer") {
-			GenerateKmer generate_kmer(argc - 1, argv + 1);
-			generate_kmer.Run();
-		}
+		//else if (command == "GenerateKmer") {
+		//	GenerateKmer generate_kmer(argc - 1, argv + 1);
+		//	generate_kmer.Run();
+		//}
 	}
 
 	return 0;
